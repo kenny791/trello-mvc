77f0fe64b5614023ace036a24837497be61070a8
Kenny Lai <kennylai@Kennys-MacBook-Pro.local>
2022-10-20 13:51:30 +1100

    creating models

---
+++ models/card.py
@@ -0,0 +1,11 @@
+from main import db
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    date = db.Column(db.Date)
+    status = db.Column(db.String)
+    priority = db.Column(db.String)
