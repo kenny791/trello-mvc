0a18026bcb147e0fa0f3050a0504654ef9ca371c
Kenny Lai <kennylai@Kennys-MacBook-Pro.local>
2022-10-20 12:36:49 +1100

    wrapping all into 'create_app' function

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ import os
 def create_app():
     app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+    db = SQLAlchemy(app)
+
+    @app.route('/')
+    def index():
+        return 'Hello World'
+
+    return app
+
 
